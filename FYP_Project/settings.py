e3a98ff3085a896929eeaf5a6a3298042d0575d2
CC <c18056180805@163.com>
2024-04-11 10:00:41 +0100

    开始部署

--- FYP_Project/settings.py
+++ FYP_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-e*u7&x$495)s4c9klqhxe(ks=sqier_uy(d7qdiahnll27sg8*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FYP_Project.wsgi --log-file -
