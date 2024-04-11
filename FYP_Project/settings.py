2cba815821d33a5b5e5b9a2f28b934434b21e758
CC <c18056180805@163.com>
2024-04-11 23:07:04 +0100

    Update database configuration

--- FYP_Project/settings.py
+++ FYP_Project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e*u7&x$495)s4c9klqhxe(ks=sqier_uy(d7qdiahnll27sg8*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
