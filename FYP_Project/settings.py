1a9edbbffe047307276e6f9d64c39ae35935c232
CC <c18056180805@163.com>
2024-04-16 03:06:47 +0100

    new version after a lot of change (but failed)

--- FYP_Project/settings.py
+++ FYP_Project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 import os
 from pathlib import Path
 
@@ -18,18 +17,11 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-e*u7&x$495)s4c9klqhxe(ks=sqier_uy(d7qdiahnll27sg8*"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
-
-
 
 INSTALLED_APPS = [
     "django.contrib.admin",
