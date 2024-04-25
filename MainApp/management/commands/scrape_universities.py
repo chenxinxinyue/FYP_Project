93e9f23019e3842f89e135c963d479931714fee3
CC <c18056180805@163.com>
2024-04-25 20:31:16 +0100

    更新 scrape_universities.py

--- MainApp/management/commands/scrape_universities.py
+++ MainApp/management/commands/scrape_universities.py
@@ -1,7 +1,11 @@
+"""
+    use Django and Selenium to scrape the university names from the website
+
+"""
 import os
 
 from django.core.management import BaseCommand
-from selenium import webdriver
+from selenium import webdriver  # Part of the Selenium library that automates web page operations
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -12,6 +16,16 @@ from MainApp.models import School
 
 
 def get_driver():
+    """
+    This function config and returns a Selenium WebDriver object
+    that controls the Chrome browser.
+    The path determine the location of the Chrome executable and Chromedriver. (in heroku)
+    Configuration includes enabling headless mode and some other options to
+    optimize the runtime environment, improve performance and compatibility
+    during automated testing or data scraping
+    :return:
+    """
+    # from heroku
     chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', 'chromedriver')
     chrome_driver_path = os.environ.get('CHROMEDRIVER_PATH', '/app/.chromedriver/bin/chromedriver')
 
@@ -30,16 +44,29 @@ def get_driver():
 
 
 def get_university_names(driver, url):
+    """
+    Load a new page with the webdriver, then set up the wait function and keep checking
+    until there is at least one element that meets the condition
+    :param driver:
+    :param url:
+    :return:
+    """
     driver.get(url)
     wait = WebDriverWait(driver, 10)  # Use WebDriverWait
     university_elements = wait.until(
+        # # univListingCards > div:nth-child(13) > div > div > div.card-wrap > div.card-body >
+        # div.uni-det > h2 > a:nth-child(2) > span
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.uni-det h2 a:nth-child(2) > span')))
     universities = [uni.text.strip() for uni in university_elements]
     return universities
 
 
 class Command(BaseCommand):
-    help = 'Scrape university data and save/update it in a CSV file'
+    help = 'Scrape university data'
+
+    """
+    handle(): the entry point for the command
+    """
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Starting to scrape university data...'))
@@ -50,7 +77,7 @@ class Command(BaseCommand):
         driver = get_driver()
         base_url = 'https://www.topuniversities.com/universities'
         all_universities = get_university_names(driver, base_url)
-
+        # #alt-style-pagination > li.active > span
         pagination_elements = WebDriverWait(driver, 10).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#alt-style-pagination li a.page-link'))
         )
